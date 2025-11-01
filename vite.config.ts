68bd8542d3ce80db6f3ec89e0ff8d423905253b0
Giorgos Bithas <giorgos_bithas@hotmail.com>
2025-11-01 15:52:24 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc"; // 👈 αυτό λείπει
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), // απαραίτητο για React/TSX
+    tailwindcss(), // Tailwind
+  ],
   base: "/react-movie-explorer/", // απαραίτητο για GitHub Pages
 });
